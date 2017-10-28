e4946707594c65b96f3db3080a905b0f36ae68dc
Your Name <you@example.com>
2017-10-28 12:51:03 +0200

    Initial commit

---
+++ src/main/java/pl/sdacademy/springsecurity/Application.java
@@ -0,0 +1,10 @@
+package pl.sdacademy.springsecurity;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+
+    }
+}
